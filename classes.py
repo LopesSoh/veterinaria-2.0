de55d512a5948ce896759b01b0fb30402c0faae3
LopesSoh <sophiac.lopes2008@gmail.com>
2025-09-02 15:50:03 -0300

    criação classe animal e apliação herança no filhos

--- classes.py
+++ classes.py
@@ -1,18 +1,28 @@
-class Gato:
+class Animal:
+    def __init__(self, nome, peso, idade, raca, cor, dono):
+        self.__nome = nome
+        self.__peso = peso
+        self.__idade = idade
+        self.__raca = raca
+        self.__cor = cor
+        self.__dono = dono
+
+
+class Gato (Animal):
 
     #modulo de controle
-    def __init__(self, __nome, __peso, __idade, __raca, __cor, __dono):
-        self.nome = __nome
-        self.peso = __peso
-        self.idade = __idade
-        self.raca = __raca
-        self.cor = __cor
-        self.dono = __dono
+    def __init__(self, nome, peso, idade, raca, cor, dono):
+        self.__nome = nome
+        self.__peso = peso
+        self.__idade = idade
+        self.__raca = raca
+        self.__cor = cor
+        self.__dono = dono
 
     def miar (self):
         print('miau miau')
 
-class Cachorro:
+class Cachorro (Animal):
 
     #modulo de controle
     def __init__(self, __nome, __peso, __idade, __raca, __cor, __dono):
@@ -26,7 +36,7 @@ class Cachorro:
     def miar (self):
         print('miau miau')
 
-class Passaro:
+class Passaro (Animal):
         
     def __init__(self, __nome, __peso, __idade, __especie, __cor, __dono):
         self.nome = __nome
