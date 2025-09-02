4cecc4c45eeec1277cfb6a7eb220267b9024906e
LopesSoh <sophiac.lopes2008@gmail.com>
2025-09-02 16:07:56 -0300

    herdando o init

--- classes.py
+++ classes.py
@@ -12,7 +12,7 @@ class Gato (Animal):
 
     #modulo de controle
     def __init__(self, nome, peso, idade, raca, cor, dono):
-        pass
+        Animal.__init__(self, nome, peso, idade, raca, cor, dono)
 
     def miar (self):
         print('miau miau')
@@ -20,26 +20,16 @@ class Gato (Animal):
 class Cachorro (Animal):
 
     #modulo de controle
-    def __init__(self, __nome, __peso, __idade, __raca, __cor, __dono):
-        self.nome = __nome
-        self.peso = __peso
-        self.idade = __idade
-        self.raca = __raca
-        self.cor = __cor
-        self.dono = __dono
+    def __init__(self, nome, peso, idade, raca, cor, dono):
+        Animal.__init__(self, nome, peso, idade, raca, cor, dono)
     
     def miar (self):
         print('miau miau')
 
 class Passaro (Animal):
         
-    def __init__(self, __nome, __peso, __idade, __especie, __cor, __dono):
-        self.nome = __nome
-        self.peso = __peso
-        self.idade = __idade
-        self.especie = __especie
-        self.cor = __cor
-        self.dono = __dono
+    def __init__(self, nome, peso, idade, raca, cor, dono):
+        Animal.__init__(self, nome, peso, idade, raca, cor, dono)
 
     def piar (self):
         print('piu piu')
